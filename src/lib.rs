1f45945d1bbd472ed989f95327b497a96098d1e1
Thomas <thomas.montaigu@laposte.net>
2017-04-28 16:47:19 +0200

    man, rust borrowind system is hard

--- src/lib.rs
+++ src/lib.rs
@@ -54,3 +54,55 @@ where T: Clone + Hash + Eq , K: Eq + Ord
 
     kruskal_mst_ordered(&vertices, &edges_ordered)
 }
+
+
+pub fn prim_mst<T, K>(vertices: &Vec<T>, edges: &HashMap<(T,T), K>)
+where T: Clone + Hash + Eq + std::fmt::Display
+{
+    let mut adjacency_list = HashMap::<T,Vec<T>>::new();
+    for (edge, _) in edges {
+        let (v1, v2) = edge.clone();
+
+        {
+            let mut neighbours_v1 = adjacency_list.entry(v1.clone()).or_insert(Vec::<T>::new());
+            neighbours_v1.push(v2.clone());
+        }
+
+        {
+            let mut neighbours_v2 = adjacency_list.entry(v2).or_insert(Vec::<T>::new());
+            neighbours_v2.push(v1.clone());
+        }
+    }
+
+    // let mut t_vertices : Vec<T> = Vec::with_capacity(vertices.len());
+    // let mut t_edges : Vec<(T, T)> = Vec::new();
+    // t_vertices.push(vertices[0].clone());
+
+
+    // let mut considered_edges : Vec<((T, T), K)> = Vec::new();
+    // for vertice in &t_vertices {
+    //     match adjacency_list.get(vertice) {
+    //         Some(vec) => {
+    //             for e in vec {
+    //                 let (v1, v2) = (vertice.clone(), e.clone());
+    //                 match edges.get(&(v1, v2)) {
+    //                     Some(value) => considered_edges.push( ((v1, v2), value) ),
+    //                     None => {},
+    //                 }
+    //             }
+    //         },
+    //         None => {}
+    //     }
+    // }
+
+
+}
+
+fn create_adjacency_hash<T, K>(edges: &HashMap<(T,T), K>) -> HashMap<T,Vec<T>>
+where T: Eq + Hash
+{
+    let mut adjacency_hash = HashMap::<T, Vec<T>>::new();
+
+
+    adjacency_hash
+}
